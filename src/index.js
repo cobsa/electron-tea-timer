162986719e92d67de85fb3455110676be50a0a07
Jaakko Koponen <cobsa89@gmail.com>
2017-10-31 17:55:51 +0200

    Initial setup with electron, react, babel, webpack

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import ReactDom from 'react-dom'
+import React from 'react'
+
+
+var app = document.getElementById('app')
+ReactDom.render(
+    <div>
+        Hello React webpack
+    </div>
+, app)
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var path = require('path')
+module.exports = {
+    entry: {
+        app: ["./src/index.js"]
+    },
+    output: {
+        path: path.resolve(__dirname, "build"),
+        publicPath: "/assets/",
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
+        ]
+    },
+    devServer: {
+        historyApiFallback: true
+    }
+};
